b84aa6ea208b25765ce8175da06de30294ac8049
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-09-15 01:18:31 -0300

    feat: add section layers

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,14 +11,20 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <meta charSet="utf-8" />
-          <meta property="og:title" content="etiquete.me" />
+          <meta property="og:title" content="etiquete.me serviços" />
           <meta
             property="og:description"
-            content="bem vindos ao etiquete.me!"
+            content="Página de mapeamento or QRCode"
           />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://etiquete.me/" />
-          <meta property="og:image" content="https://etiquete.me/ogimage.png" />
+          <meta
+            property="og:image"
+            content="https://etiquete.me/ogimage.jpeg"
+          />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:width" content="620" />
+          <meta property="og:image:height" content="320" />
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
